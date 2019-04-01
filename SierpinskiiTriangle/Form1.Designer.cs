21b0edf01411bb98ca75eea22c4e28315269a4f4
Your Name <you@example.com>
2019-04-01 20:48:07 +0300

    Sierpinski triangle

---
+++ SierpinskiiTriangle/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Sierpinski
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TriangleButton = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TriangleButton
+            // 
+            this.TriangleButton.ForeColor = System.Drawing.Color.Red;
+            this.TriangleButton.Location = new System.Drawing.Point(447, 0);
+            this.TriangleButton.Margin = new System.Windows.Forms.Padding(4, 4, 7, 7);
+            this.TriangleButton.Name = "TriangleButton";
+            this.TriangleButton.Size = new System.Drawing.Size(133, 40);
+            this.TriangleButton.TabIndex = 0;
+            this.TriangleButton.Text = "Build triangle";
+            this.TriangleButton.UseVisualStyleBackColor = true;
+            this.TriangleButton.Click += new System.EventHandler(this.TriangleButton_Click);
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(29, 60);
+            this.PictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(733, 677);
+            this.PictureBox.TabIndex = 2;
+            this.PictureBox.TabStop = false;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.ForeColor = System.Drawing.Color.Red;
+            this.label.Location = new System.Drawing.Point(26, 12);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(298, 17);
+            this.label.TabIndex = 3;
+            this.label.Text = "Enter step to build Sierpinski triangle (max=8):";
+            this.label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox.Location = new System.Drawing.Point(329, 7);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(62, 22);
+            this.textBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.TriangleButton);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Sierpinski triangle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Button TriangleButton;
+
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox;
+
+
+
+    }
+}
---
+++ SierpinskiiTriangle/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sierpinski
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap Bitmap;
+
+        private Graphics Graphics;
+
+        private int Step;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void TriangleButton_Click(object sender, EventArgs e)
+        {
+            Step = Convert.ToInt32(textBox.Text);
+            Bitmap = new Bitmap(PictureBox.Width, PictureBox.Height);
+            Graphics = Graphics.FromImage(Bitmap);
+
+            PointF LeftPoint = new PointF(0, Bitmap.Height);
+            PointF RightPoint = new PointF(Bitmap.Width, Bitmap.Height);
+            PointF TopPoint = new PointF(Bitmap.Width / 2f, 0);
+
+            BuildTriangle(Step, TopPoint, LeftPoint, RightPoint);
+
+            PictureBox.BackgroundImage = Bitmap;
+        }
+
+
+
+        private void BuildTriangle(int Step, PointF TopPoint, PointF LeftPoint, PointF RightPoint)
+        {
+            if (Step == 0)
+            {
+                PointF[] triangle = new PointF[3] { TopPoint, LeftPoint, RightPoint };
+                Graphics.FillPolygon(Brushes.Red, triangle);
+
+            }
+
+            if (Step > 0)
+            {
+
+                PointF MiddleLeft = new PointF((TopPoint.X + LeftPoint.X) / 2f, (TopPoint.Y + LeftPoint.Y) / 2f);
+                PointF MiddleRight = new PointF((TopPoint.X + RightPoint.X) / 2f, (TopPoint.Y + RightPoint.Y) / 2f);
+                PointF MiddleTop = new PointF((RightPoint.X + LeftPoint.X) / 2f, (RightPoint.Y + LeftPoint.Y) / 2f);
+
+                BuildTriangle(Step - 1, TopPoint, MiddleLeft, MiddleRight);
+                BuildTriangle(Step - 1, MiddleLeft, LeftPoint, MiddleTop);
+                BuildTriangle(Step - 1, MiddleRight, MiddleTop, RightPoint);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
